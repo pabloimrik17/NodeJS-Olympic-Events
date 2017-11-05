c1daff9b79853be6ad5f5d7922ee03e98d0cf461
Etherless <pabloimrik17@gmail.com>
2017-11-05 02:06:22 +0100

    Events: -- Page added -- Route added -- Controller added

---
+++ app/controllers/events.controller.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    showEvents: (req, res) => {
+        const events = [
+            { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' },
+            { name: 'Swimming', slug: 'swimming', description: 'Michael Phelps is the fast fish.' },
+            { name: 'Weightlifting', slug: 'weightlifting', description: 'Lifting heavy things up' }
+        ];
+
+        res.render('pages/events', { events: events});
+    }
+
+};
--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const mainController = require('./controllers/main.controller')
+const eventsController = require('./controllers/events.controller')
+
 
 router.get('/', mainController.showHome);
+router.get('/events', eventsController.showEvents);
 
 module.exports = router;
