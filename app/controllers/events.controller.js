4409c287e656c03b3f22c89afd686bbced5a3186
Etherless <pabloimrik17@gmail.com>
2017-11-05 03:52:23 +0100

    Event Controller: -- Refact. -- Getting events from mongodb

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,36 +1,38 @@
 const Event = require('../models/event.model');
 
 module.exports = {
-
-    showEvents: (req, res) => {
-
-
-        res.render('pages/events', { events: {}});
-    },
-
-    showEvent: (req, res) => {
-        const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' };
-
-        res.render('pages/event', { event: event});
-    },
-
-    seedEvents: (req, res) => {
-
-        const events = [
-            { name: 'Basketball', description: 'Throwing into a basket.' },
-            { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
-            { name: 'Weightlifting', description: 'Lifting heavy things up' },
-            { name: 'Ping Pong', description: 'Super fast paddles' }
-        ];
-
-        Event.remove({}, () => {
-            for (event of events) {
-                var newEvent = new Event(event);
-                newEvent.save();
-            }
-        });
-
-        res.send("Database seeded!");
-    }
-
+    showEvents: showEvents,
+    showEvent: showEvent,
+    seedEvents: seedEvents
 };
+
+function showEvents(req, res) {
+    Event.find({}, (err, events) => {
+        res.render('pages/events', {events: events});
+    })
+}
+
+function showEvent(req, res) {
+    const event = {name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'};
+
+    res.render('pages/event', {event: event});
+}
+
+function seedEvents(req, res) {
+
+    const events = [
+        {name: 'Basketball', description: 'Throwing into a basket.'},
+        {name: 'Swimming', description: 'Michael Phelps is the fast fish.'},
+        {name: 'Weightlifting', description: 'Lifting heavy things up'},
+        {name: 'Ping Pong', description: 'Super fast paddles'}
+    ];
+
+    Event.remove({}, () => {
+        for (event of events) {
+            var newEvent = new Event(event);
+            newEvent.save();
+        }
+    });
+
+    res.send("Database seeded!");
+}
